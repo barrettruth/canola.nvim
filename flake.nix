81dc09fea9632900314aa53be3b7ffb7d8efafdd
Barrett Ruth <br.barrettruth@gmail.com>
2026-03-19 17:54:39 -0400

    ci: add vimdoc-language-server check
    Problem: no CI validation that vimdoc files under `doc/` are well-formed.
    
    Solution: add `vimdoc-language-server` flake input, include it in the
    devShell, and wire a `vimdoc-check` job in `quality.yaml` gated on
    `doc/**` path changes. Also add the check to `scripts/ci.sh`.

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
+    vimdoc-language-server.url = "github:barrettruth/vimdoc-language-server";
   };
 
   outputs =
     {
       nixpkgs,
       systems,
+      vimdoc-language-server,
       ...
     }:
     let
@@ -25,6 +27,7 @@
             pkgs.prettier
             pkgs.stylua
             pkgs.selene
+            vimdoc-language-server.packages.${pkgs.system}.default
             (pkgs.luajit.withPackages (ps: [
               ps.busted
               ps.nlua
